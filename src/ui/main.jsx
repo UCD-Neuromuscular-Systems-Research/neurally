64cebdeb1e6e1c63b7819bfb14eabb2bc74ee405
krishnateja <krishnateja.jinka@ucdconnect.ie>
2025-07-26 11:27:34 +0100

    fix: refactor main.jsx to clean up stuff from rebases
    Multiple rebases were implemented on main to clean up few commit messages and it led to few unexpected remains. Cleaned that up

--- src/ui/main.jsx
+++ src/ui/main.jsx
@@ -1,18 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-<<<<<<< HEAD
-import App from './App.jsx';
-=======
 import Home from './components/Home.jsx';
 import Dashboard from './components/Dashboard.jsx';
-import App from './App.jsx';
 import Results from './components/Results.jsx';
+import { createMemoryRouter, RouterProvider } from 'react-router';
 
 const router = createMemoryRouter([
   {
     path: '/',
-    Component: App,
+    Component: Home,
   },
   {
     path: 'tests/:testType',
@@ -23,10 +20,9 @@ const router = createMemoryRouter([
     Component: Results,
   },
 ]);
->>>>>>> d67f0f8 (style: add UI with dummy data for ref)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
