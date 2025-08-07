d606dfb1a693dc06b182f97a6f6fe1c361572f1d
krishnateja <Krishnatejajinka@gmail.com>
2025-08-07 22:59:51 +0100

    refactor: use preload path from above executed method

--- src/electron/main.js
+++ src/electron/main.js
@@ -91,7 +91,7 @@ const createMainWindow = () => {
     // fullscreen: true,
     resizable: false,
     webPreferences: {
-      preload: getPreloadPath(),
+      preload: preloadPath,
     },
   });
 
